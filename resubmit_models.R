df8c84bbb0c9ec631a3767c3e17087928dd8fcc8
rrchai <73901500+rrchai@users.noreply.github.com>
2023-03-22 12:06:02 -0400

    resubmit models

---
+++ resubmit_models.R
@@ -0,0 +1,119 @@
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(stringr)
+})
+
+source("utils.R")
+
+# set up synapse
+reticulate::use_condaenv('synapse')
+synapseclient <- reticulate::import('synapseclient')
+challengeutils <- reticulate::import("challengeutils")
+syn <- synapseclient$Synapse()
+syn$login(silent = TRUE)
+
+
+
+# TODO: set better args
+dry_run <- FALSE
+view_id <- "syn51157023"
+tasks <- list(task1 = list(old_eval = "9615023", new_eval = "9615324"),
+              task2 = list(old_eval = "9615024", new_eval = "9615302"))
+args <- commandArgs(trailingOnly=TRUE)
+task <- args[1]
+stopifnot(task %in% c("task1", "task2"))
+phase <- args[2]
+stopifnot(phase %in% c("public", "private"))
+
+# get ids of all scored submissions
+eval_id <- tasks[[task]]$old_eval
+task_sub_df <- get_ranked_submissions(syn, view_id, eval_id, phase)
+
+# validate if submitter is overlapped with existing teams
+all_users <- unique(task_sub_df$submitterid)
+all_teams <- all_users[sapply(all_users, is_team, syn = syn)]
+is_dup_user <- validate_users(syn,
+                              users = all_users,
+                              teams = all_teams,
+                              .drop = TRUE)
+
+if (length(is_dup_user) > 0) {
+  message(
+    "Submissions from submitters will not be re-submitted:\n",
+    paste(task_sub_df$team[match(names(is_dup_user), task_sub_df$submitterid)], collapse = '", "'),
+    "\nbecause of duplicated with exiting teams"
+  )
+}
+
+# remove duplicated user and get the best submission of a team/user
+task_sub_df <- task_sub_df %>%
+  filter(!submitterid %in% names(is_dup_user),
+         !team %in% c("scRNA-seq and scATAC-seq Data Analysis DREAM Challenge Organizers", "rchai")) %>%
+  mutate(overall_rank = as.numeric(overall_rank)) %>%
+  group_by(team) %>%
+  slice_min(overall_rank, n = 1) %>%
+  arrange(overall_rank)
+
+if (nrow(task_sub_df) > 0) {
+
+  message("\nRe-submitting submissions from evluation ", dQuote(eval_id), " and ", phase, " phase ...")
+
+  sub_ids <- unique(task_sub_df$id)
+  for (n in seq_along(sub_ids)[1]) {
+
+    message(str_glue("Submitting {dQuote(sub_ids[n])} ({n} of {nrow(task_sub_df)}) ..."))
+
+    if (!dry_run) {
+
+      # this approach not works on the project lack of access
+      # submitted <- resubmit(syn, sub_ids[n], tasks[[task]]$new_eval)
+      
+      # copy images to staging then submit
+      staging_project_id <- "syn26720921"
+      sub <- syn$getSubmission(sub_ids[n])
+      sub_image <- str_glue("{sub$dockerRepositoryName}@{sub$dockerDigest}")
+      image_tag <- str_glue("{sub$id}_{phase}")
+
+      image <- copy_model(image = sub_image,
+                          project_id = staging_project_id,
+                          name = "post-submissions",
+                          tag = image_tag)
+
+      repo_entity <- syn$restGET(str_glue('/entity/dockerRepo/id?repositoryName={image$repo_name}'))
+
+      submitted <- syn$submit(
+        evaluation = tasks[[task]]$new_eval,
+        entity = repo_entity$id,
+        name = sub$id,
+        dockerTag = image_tag
+      )
+      
+      # wait 15 min to check if submission is scored
+      # cp <- 0
+      # while (cp <= 0) {
+      #   Sys.sleep(15 * 60)
+      #   status <- syn$getSubmissionStatus(submitted$id)
+      #   is_score <- tryCatch(
+      #     {
+      #       res <- status$submissionAnnotations$submission_status == "SCORED" || 
+      #         status$status == "INVALID"
+      #       return(res)
+      #     },
+      #     error = function(e) FALSE
+      #   )
+      #   if (is_score) cp <- cp + 1
+      # }
+      Sys.sleep(60 * 60)
+      
+      message("Annotating original submitterid and submission phase ... ")
+      # update annots with origin submitterid and phase
+      # idk why submitterid cannot be overwrote, so use new column
+      team_id <- task_sub_df$submitterid[match(sub_ids[n], task_sub_df$id)]
+      annots <- list(
+        re_submission_phase = as.character(phase),
+        re_submitterid = as.character(team_id)
+      )
+      annot_res <- challengeutils$annotations$annotate_submission(syn, submitted$id, annots)
+    }
+  }
+}

2e7bf2f9ab41ffdcc1fd18393c91c13cd42e8b08
rrchai <73901500+rrchai@users.noreply.github.com>
2023-11-01 02:33:38 -0400

    add downstream analysis on task1

---
+++ downstream/downstream_funcs.R
@@ -0,0 +1,244 @@
+aggregate_output_by_mean<- function(data_path_list, new_filename) {
+
+  data_list <- lapply(data_path_list, function(f) {
+    fread(f, verbose = FALSE) %>% tibble::column_to_rownames("V1")
+  })
+  shared_cells <- Reduce(intersect, lapply(data_list, colnames))
+  shared_genes <- Reduce(intersect, lapply(data_list, rownames))
+
+  data_list <- lapply(data_list, function(f) {
+    f <- f[shared_genes, shared_cells]
+    f %>% tibble::rownames_to_column("V1") %>%
+      as.data.table() %>%
+      melt.data.table(id.vars = 1) %>% 
+        setnames(c("genes", "cells", "values"))
+  })
+      
+  new_data <- rbindlist(data_list, use.names = TRUE) %>%
+    .[, .(values = mean(values)), by = .(cells, genes)] %>%
+    dcast(genes ~ cells, value.var = "values") %>%
+    tibble::column_to_rownames("genes")
+
+  fwrite(new_data, new_filename, row.names = TRUE)
+  return(new_data)
+}
+
+# Prepare seurat object from 10x style matrices
+matrices_to_seurat <- function(data_path, nickname, dataset, cells, data_type, condition, npcs = 10){
+  # create output directory if missing
+  
+  if(!file.exists('seurat_objects')){
+    dir.create('seurat_objects')
+  }
+  
+  if(!file.exists('plots')){
+    dir.create('plots')
+  }
+  
+  # Build output file name
+  output_filename <- paste0('seurat_objects/', nickname, '.rds')
+  
+  # Check if file already exists and/or was recorded in metadata
+  # if(file.exists(output_filename)){
+  #   print('Error: RDS file already exists with this name')
+  # }else{
+    print('Processing Seurat object - ETA ~5min')
+    
+    # read in matrix and convert to seurat object
+    # raw <- Read10X(data.dir = data_folder)
+    raw <- data.table::fread(data_path, data.table = FALSE) %>% tibble::column_to_rownames("V1")
+    so <- CreateSeuratObject(counts = raw,
+                             project = nickname)
+    
+    # Subset to just cells in reference/10X 'filtered' list
+    so <- subset(so,
+                 cells = cells)
+    
+    # create cell_barcode metadata
+    
+    so@meta.data$cell_barcode <- row.names(so@meta.data)
+
+    # Process seurat object with standard workflow
+    so <- NormalizeData(so, verbose = FALSE) %>%
+      FindVariableFeatures(nfeatures = 3000, verbose = FALSE) %>%
+      ScaleData(verbose = FALSE) %>%
+      RunPCA(verbose = FALSE) %>%
+      RunUMAP(reduction = 'pca', dims = 1:npcs, verbose = FALSE) %>%
+      FindNeighbors(reduction = 'pca', dims = 1:npcs, verbose = FALSE) %>%
+      FindClusters(resolution = seq(from = 0.1, to = 1, by = 0.1), verbose = FALSE)
+    
+    # Save umap colored by nCount_UMI
+    # curr_umap <- FeaturePlot(so, 
+    #                          features = 'nCount_RNA')+
+    #   ggtitle(nickname)+
+    #   coord_equal()
+    
+    # ggsave(filename = paste0('plots/', nickname, '_umap_by_counts.pdf'),
+    #        plot = curr_umap)
+    
+    # Save metadata for seurat object
+    
+    meta <- tibble(file = data_path,
+                   nickname = nickname,
+                   dataset = dataset,
+                   data_type = data_type,
+                   condition = condition,
+                   date_created = Sys.time(),
+                   output_file = output_filename,
+                   npcs = npcs)
+    
+    
+    if(!file.exists('so_metadata.csv')){
+      # Save seurat object & metadata
+      saveRDS(so, file = output_filename)
+      write_csv(meta, 'so_metadata.csv')
+    }else{
+      prior_meta <- read_csv('so_metadata.csv')
+      
+      if(meta$nickname %in% prior_meta$nickname){
+        prior_meta <- prior_meta[-which(prior_meta$nickname == meta$nickname), ]
+      }
+
+      new_meta <- rbind(prior_meta,
+                        meta)
+      
+      # Save seurat object & metadata
+      saveRDS(so, file = output_filename)
+      write_csv(new_meta, 'so_metadata.csv')
+    
+    return(so)
+  }
+  
+}
+
+
+
+compare_so_to_ref <- function(query,
+                              reference){
+  # match genes/cells
+  shared_genes <-  intersect(rownames(query), rownames(reference))
+  shared_cells <-  intersect(colnames(query), colnames(reference))
+  reference@assays$RNA@data <- reference@assays$RNA@data[shared_genes, shared_cells]
+  reference@assays$RNA@counts <- reference@assays$RNA@counts[shared_genes, shared_cells]
+  query@assays$RNA@data <- query@assays$RNA@data[shared_genes, shared_cells]
+  query@assays$RNA@counts <- query@assays$RNA@counts[shared_genes, shared_cells]
+
+
+  if(sum(colnames(query) == colnames(reference))/ncol(reference) != 1){
+    print('Error - cell barcodes do not line up between query and reference data set')
+  }
+  
+  # Clustering similarity (NMI and ARI)
+  paired_cluster_resolutions <- intersect(colnames(query@meta.data),
+                                          colnames(reference@meta.data)) %>%
+    grep(., pattern = '^RNA_snn_res.', value = TRUE)
+  
+  
+  for(i in paired_cluster_resolutions){
+    curr_nmi <- aricode::NMI(c1 = query@meta.data[query@meta.data$cell_barcode %in% shared_cells, colnames(query@meta.data)==i],
+                             c2 = reference@meta.data[reference@meta.data$cell_barcode %in% shared_cells, colnames(reference@meta.data) == i])
+    
+    curr_ari <- aricode::ARI(c1 = query@meta.data[query@meta.data$cell_barcode %in% shared_cells, colnames(query@meta.data)==i],
+                             c2 = reference@meta.data[reference@meta.data$cell_barcode %in% shared_cells, colnames(reference@meta.data) == i])
+    
+    clust_tibble <- tibble(query_name = query@project.name,
+                           reference_name = reference@project.name,
+                           resolution = as.numeric(str_remove(i, pattern = '^RNA_snn_res.')),
+                           ari = curr_ari,
+                           nmi = curr_nmi,
+                           n_clusters.query = length(unique(query@meta.data[query@meta.data$cell_barcode %in% shared_cells, colnames(query@meta.data)==i])),
+                           n_clusters.ref = length(unique(reference@meta.data[query@meta.data$cell_barcode %in% shared_cells, colnames(reference@meta.data) == i])))
+    
+    if(i == paired_cluster_resolutions[1]){
+      cluster_metrics <- clust_tibble
+    }else{
+      cluster_metrics <- rbind(cluster_metrics,
+                               clust_tibble)
+    }
+  }
+  
+  # KNN overlap
+  unique_barcodes <- colnames(reference)
+  for(i in unique_barcodes){
+    shared_nn <- sum(reference@graphs$RNA_nn[,i] & query@graphs$RNA_nn[,i])
+    union_nn <- sum(reference@graphs$RNA_nn[,i] | query@graphs$RNA_nn[,i])
+    
+    curr_knn_tibble <- tibble(query_name = query@project.name,
+                              reference_name = reference@project.name,
+                              barcode = i,
+                              shared_nn = shared_nn,
+                              union_nn = union_nn,
+                              jaccard_shared = shared_nn/union_nn)
+    
+    if(i == unique_barcodes[1]){
+      knn_tibble <- curr_knn_tibble
+    }else{
+      knn_tibble <- rbind(knn_tibble,
+                          curr_knn_tibble)
+    }
+  }
+  
+  knn_summary <- tibble(query_name = query@project.name,
+                        reference_name = reference@project.name,
+                        knn_jaccard_mean = mean(knn_tibble$jaccard_shared))
+  
+  # Drop out comparison
+  
+  tp <- sum(reference@assays$RNA@counts == 0 & query@assays$RNA@counts == 0)
+  fn <- sum(reference@assays$RNA@counts == 0 & query@assays$RNA@counts != 0)
+  fp <- sum(reference@assays$RNA@counts != 0 & query@assays$RNA@counts == 0)
+  tn <- sum(reference@assays$RNA@counts != 0 & query@assays$RNA@counts != 0)
+  
+  f1 <- 2*tp/(2*tp+fp+fn)
+  
+  drop_outs <- tibble(query_name = query@project.name,
+                      reference_name = reference@project.name,
+                      tp = tp,
+                      fp = fp,
+                      tn = tn,
+                      fn = fn,
+                      f1 = f1)
+  
+  metrics <- list(cluster_metrics = cluster_metrics,
+                  knn_tibble = knn_tibble,
+                  knn_summary = knn_summary,
+                  drop_outs = drop_outs)
+  
+  return(metrics)
+}
+
+
+
+lfc_comparison <- function(comparsion){
+  
+  perturb_nickname = comparsion[1]
+  control_nickname = comparsion[2]
+  
+  print(str_glue("Running DE analysis on {toString(perturb_nickname)} vs {toString(control_nickname)}"))
+  so_prtb <- readRDS(file.path("seurat_objects",  str_glue("{perturb_nickname}.rds")))
+  so_ctrl <- readRDS(file.path("seurat_objects",  str_glue("{control_nickname}.rds")))
+  
+  # Add condition metadata
+  so_prtb@meta.data$condition <- 'perturbation'
+  so_ctrl@meta.data$condition <- 'control'
+  
+  # Merge to a single seurat object
+  so_merge <- merge(x = so_prtb,
+                    y = so_ctrl)
+  
+  Idents(so_merge) <- 'condition'
+  
+  # Normalize merged seurat object
+  so_merge <- NormalizeData(so_merge, verbose = FALSE)
+
+  # Find markers with default wilcox test
+  lfc <- FindMarkers(so_merge,
+                      ident.1 = 'perturbation',
+                      ident.2 = 'control',
+                      verbose = FALSE) %>%
+    mutate(gene = row.names(.)) %>%
+    mutate(ident.1 = perturb_nickname) %>%
+    mutate(ident.2 = control_nickname)
+  
+  return(lfc)
+}
---
+++ downstream/downstream_task1.R
@@ -0,0 +1,317 @@
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(stringr)
+  library(Seurat)
+  library(data.table)
+  library(magrittr)
+  library(readr)
+  library(tidyr)
+  library(ggplot2)
+  library(aricode)
+  library(parallel)
+  library(pbmcapply)
+  library(future)
+})
+
+plan("multiprocess", workers = 6) # set up seurat funcs parallelization
+ncores <- 24 # cores used to run analysis on files in parallel
+
+source("/downstream/downstream_funcs.R")
+options(readr.show_col_types = FALSE)
+
+## Set up data paths
+raw_dir <- "/challenge-data/raw/"
+ds_dir <-  "/challenge-data/downsampled/scrna/"
+output_dir <- "/output"
+
+# Set up datasets info
+# teams <- c("GOAL_LAB", "DLS5", "zoradeng_post", 
+#            "BBKCS_post", "moqri_post", "LDExplore",
+#            "USF_biostat_post", "Metformin-121")
+datasets <- c("ds1a", "ds1b", "ds1c", "ds1d", "ds2", "ds3")
+ds_reads_props <- c("p10k", "p20k", "p50k")
+ds_cells_props <- c("p00625", "p0125", "p025")
+
+# Read team input
+team <- commandArgs(trailingOnly = TRUE)[1]
+
+# Read valid cells
+valid_cell_path <- file.path(output_dir, "valid_cells.rds")
+if (!file.exists(valid_cell_path)) {
+  valid_cells <- lapply(datasets, function(dataset) {
+    read_tsv(file.path(raw_dir, dataset, "filtered_feature_bc_matrix/barcodes.tsv.gz"),
+             col_names = FALSE) %>% unlist() %>% as.character()
+  }) %>% set_names(datasets)
+  saveRDS(valid_cells, valid_cell_path)
+} else {
+  valid_cells <- readRDS(valid_cell_path)
+}  
+
+# Create reference seurat objects
+refs_path <- file.path(output_dir, "refs.rds")
+if (!file.exists(refs_path)) {
+  refs <- mclapply(datasets, function(dataset) {
+    nickname <- str_glue("{dataset}_p1")
+    f <- str_glue("{nickname}.csv")
+    matrices_to_seurat(file.path(ds_dir, f),
+                       nickname = nickname,
+                       cells = valid_cells[[dataset]],
+                       dataset = dataset,
+                       data_type = "reference",
+                       condition = "control")
+  }, mc.cores = ncores) %>% set_names(datasets)
+  saveRDS(refs, refs_path)
+} else {
+  refs <- readRDS(refs_path)
+}  
+
+
+# Compare imputed data with original data
+out_all <- list()
+for (dataset in datasets) {
+ 
+  message("Comparing ", dataset, " >>>>>>>>>>")
+
+  if (dataset == "ds2") {
+    combinations <- expand.grid(dataset, ds_cells_props)
+  } else if (dataset == "ds3") {
+    combinations1 <- expand.grid(dataset, ds_reads_props)
+    combinations2 <- expand.grid(dataset, ds_cells_props)
+    combinations <- rbind(combinations1, combinations2)
+  } else {
+    combinations <- expand.grid(dataset, ds_reads_props)
+  }
+  
+  filenames <- str_glue("{combinations$Var1}_{combinations$Var2}")
+  
+  # control_querys_path <- file.path(output_dir, "control_querys.rds")
+  # if (!file.exists(control_querys_path)) {
+  #   control_querys <- pbmclapply(filenames, function(f) {
+
+  #     data_path <- file.path(ds_dir, f)     
+
+  #     if (!is_by_cells) { # just take one of replicates if downsampled by cells
+  #       new_f_path <- paste0(data_path, "_n", 1, "_imputed.csv")
+  #     } else {
+  #     new_f_path <- aggregate_output_by_mean(data_path_list = paste0(data_path, "_n", 1:3, "_imputed.csv"),
+  #                                           new_filename = str_glue("{team}_{f}_imputed.csv"))
+  #     }
+     
+  #     matrices_to_seurat(new_f_path,
+  #                       nickname = str_glue("{f}"),
+  #                       cells = valid_cells[[dataset]],
+  #                       dataset = dataset,
+  #                       data_type = "query",
+  #                       condition = "control")
+    
+  #   }, mc.cores = ncores) %>% set_names(filenames)
+  #   saveRDS(control_querys, control_querys_path)
+  # }
+
+  querys <-  pbmclapply(filenames, function(f) {
+    print(f)
+    data_path <- file.path(output_dir, team, "output", f)
+    is_by_cells <-  str_split(basename(f), "_")[[1]][2] %in% ds_cells_props
+
+    new_filename <- str_glue("{team}_{f}_imputed.csv")
+    if (is_by_cells) { # just take one of replicates if downsampled by cells
+      imp <- fread(paste0(data_path, "_n", 1, "_imputed.csv")) %>% tibble::column_to_rownames("V1")
+      fwrite(imp, new_filename, row.names = TRUE)
+    } else {
+      new_data <- aggregate_output_by_mean(data_path_list = paste0(data_path, "_n", 1:3, "_imputed.csv"),
+                                           new_filename = new_filename)
+    }
+    
+    matrices_to_seurat(new_filename,
+                       nickname = str_glue("{team}_{f}"),
+                       cells = valid_cells[[dataset]],
+                       dataset = dataset,
+                       data_type = "query",
+                       condition = "perturb")
+  }, mc.cores = ncores) %>% set_names(filenames)
+  
+  out <- pbmclapply(seq_along(querys), function(i) {
+    # allow_cell_mismatch <- str_split(names(querys)[i], "_")[[1]][2] %in% ds_cells_props
+    compare_so_to_ref(querys[[i]], refs[[dataset]])
+  }, mc.cores = ncores) %>% set_names(filenames)
+  out_all[[dataset]] <- out
+}
+
+out_path <-  file.path(output_dir, str_glue("{team}_comparisons.rds"))
+if (!file.exists(out_path)) {
+  saveRDS(out_all, out_path)
+} 
+# out_all <- readRDS(out_path)
+
+
+# Clustering comparison ami vs nmi
+message("Generating clustering comparison plots >>>>>>>>>>")
+clustering_plot_path <- file.path(output_dir, str_glue("{team}_clustering_knn_dropouts_comparison.pdf"))
+pdf(clustering_plot_path)
+for (dataset in datasets) {
+  print(dataset)
+  clust_df <- do.call("rbind",
+                      unlist(lapply(X = out_all[[dataset]],
+                                    FUN =  "[",
+                                    'cluster_metrics'),
+                              recursive = FALSE)) 
+  
+  p1 <- ggplot(clust_df, aes(x = ari, y = nmi))+
+    geom_point(aes(size = resolution))+
+    ylim(0,1)+
+    geom_smooth(formula = y ~ x, method = "lm")+
+    theme_bw()+ 
+    facet_wrap(~query_name, scales = "free", ncol = ifelse(dataset == "ds3", 2, 1)) +
+    ggtitle("Leiden clustering overlap") +
+    labs(caption = str_glue("Dataset: {dataset}\n Team: {team}"))
+  
+  clust_df2 <- clust_df %>%
+    gather(c(ari, nmi),
+          key = 'metric',
+          value = 'value')
+
+  p2 <- ggplot(clust_df2, aes(x = resolution, y = value, color = query_name))+
+    geom_line()+
+    geom_point()+
+    facet_wrap(~metric)+
+    theme_bw()+
+    ggtitle("Dot/line plot of clustering ari/nmi") +
+    labs(caption = str_glue("Dataset: {dataset}\n Team: {team}"))
+  
+  p3 <- ggplot(clust_df2, aes(x = metric, y = value, fill = query_name))+
+    geom_boxplot()+
+    ylim(0,1) +
+    theme_bw()+
+    ggtitle("Boxplot of clustering ari/nmi") +
+    labs(caption = str_glue("Dataset: {dataset}\n Team: {team}"))
+  
+  print(p1)
+  print(p2)
+  print(p3)
+  
+  knn_df <- do.call('rbind',
+                    unlist(lapply(X = out_all[[dataset]],
+                                  FUN =  "[",
+                                  'knn_tibble'),
+                            recursive = FALSE))
+
+  p4 <- ggplot(knn_df, aes(x = query_name, y = jaccard_shared, fill = query_name))+
+    geom_violin(draw_quantiles = c(0.25,0.75))+
+    stat_summary(fun = 'mean',
+                geom = 'point',
+                color = 'black')+
+    theme_bw()+
+    ggtitle("Boxplot of KNN jaccard") +
+    labs(caption = str_glue("Dataset: {dataset}\n Team: {team}"))
+
+  print(p4)
+
+  dropouts <- do.call('rbind',
+                      unlist(lapply(X = out_all[[dataset]],
+                                    FUN =  "[",
+                                    'drop_outs'),
+                            recursive = FALSE))
+  dropout_rates <- dropouts %>%
+    mutate(n_events = tp+fp+tn+fn,
+          tp_rate = tp/n_events,
+          fp_rate = fp/n_events,
+          tn_rate = tn/n_events,
+          fn_rate = fn/n_events) %>%
+    gather(-c(query_name,
+              reference_name),
+          key = 'metric',
+          value = 'score') %>%
+    filter(!metric %in% c('tp', 'fp', 'tn', 'fn', 'n_events'))
+  
+  p5 <- ggplot(dropout_rates, aes(x = metric, y = score, color = query_name))+
+    geom_point(size = 4)+
+    geom_line(aes(group = query_name))+
+    theme_bw()+
+    ggtitle("Dot/line plot for drop out") +
+    labs(caption = str_glue("Dataset: {dataset}\n Team: {team}"))
+  print(p5)
+}
+dev.off()
+
+# Log fold-change comparisons
+message("Generating IFC comparison plots >>>>>>>>>>")
+ifc_plot_path <- file.path(output_dir, str_glue("{team}_log_fold_change_comparison.pdf"))
+pdf(ifc_plot_path)
+groups <- list(c("ds1a", "ds1b"), c("ds1c", "ds1d"))
+for (g in groups) {
+  controls <- c(str_glue("{team}_{g[1]}_{ds_reads_props}"), str_glue("{g[1]}_p1"))
+  perturbs <- c(str_glue("{team}_{g[2]}_{ds_reads_props}"), str_glue("{g[2]}_p1"))
+  comparisons_to_make <- data.frame(perturb_nicknames = perturbs,
+                                    control_nicknames = controls)
+
+  lfcs <- pbmclapply(1:nrow(comparisons_to_make), function(i) {
+    lfc_comparison(comparisons_to_make[i, ])
+  }, mc.cores = ncores) %>% set_names(gsub(str_glue("{team}_"), "", comparisons_to_make$control_nicknames))
+  
+  
+  lfc_10k <- full_join(x = lfcs[[1]],
+                        y = lfcs[[4]],
+                        by = "gene",
+                        suffix = c("_10k", "_1")) %>%
+    mutate(avg_log2FC_1 = ifelse(is.na(avg_log2FC_1), 0, avg_log2FC_1)) %>%
+    mutate(avg_log2FC_10k = ifelse(is.na(avg_log2FC_10k), 0, avg_log2FC_10k))
+
+  lfc_20k <- full_join(x = lfcs[[2]],
+                      y = lfcs[[4]],
+                      by = "gene",
+                      suffix = c("_20k", "_1")) %>%
+    mutate(avg_log2FC_1 = ifelse(is.na(avg_log2FC_1), 0, avg_log2FC_1)) %>%
+    mutate(avg_log2FC_20k = ifelse(is.na(avg_log2FC_20k), 0, avg_log2FC_20k))
+
+  lfc_50k <- full_join(x = lfcs[[3]],
+                      y = lfcs[[4]],
+                      by = "gene",
+                      suffix = c("_50k", "_1")) %>%
+    mutate(avg_log2FC_1 = ifelse(is.na(avg_log2FC_1), 0, avg_log2FC_1)) %>%
+    mutate(avg_log2FC_50k = ifelse(is.na(avg_log2FC_50k), 0, avg_log2FC_50k))
+
+  p1 <- ggplot(lfc_10k, aes(x = avg_log2FC_1, y = avg_log2FC_10k))+
+    geom_point()+
+    geom_smooth(formula = y ~ x, method = 'lm')+
+    theme_bw()+
+    coord_equal()+
+    labs(title = 'LFC correlation: 100% vs 10k reads',
+        caption = str_glue("Perturb ({g[2]}) vs Control ({g[1]}) \n Team: {team}"),
+        subtitle = paste0('Pearson cor: ', cor(lfc_10k$avg_log2FC_1, lfc_10k$avg_log2FC_10k, use = 'complete.obs')))
+
+  p2 <- ggplot(lfc_20k, aes(x = avg_log2FC_1, y = avg_log2FC_20k))+
+    geom_point()+
+    geom_smooth(formula = y ~ x, method = 'lm')+
+    theme_bw()+
+    coord_equal()+
+    labs(title = 'LFC correlation: 100% vs 20k reads',
+        caption = str_glue("Perturb ({g[2]}) vs Control ({g[1]}) \n Team: {team}"),
+        subtitle = paste0('Pearson cor: ', cor(lfc_20k$avg_log2FC_1, lfc_20k$avg_log2FC_20k, use = 'complete.obs')))
+
+  p3 <- ggplot(lfc_50k, aes(x = avg_log2FC_1, y = avg_log2FC_50k))+
+    geom_point()+
+    geom_smooth(formula = y ~ x, method = 'lm')+
+    theme_bw()+
+    coord_equal()+
+    labs(title = 'LFC correlation: 100% vs 50k reads',
+        caption = str_glue("Perturb ({g[2]}) vs Control ({g[1]}) \n Team: {team}"),
+        subtitle = paste0('Pearson cor: ', cor(lfc_50k$avg_log2FC_1, lfc_50k$avg_log2FC_50k, use = 'complete.obs')))
+  
+  print(p1)
+  print(p2)
+  print(p3)
+}
+dev.off()
+
+# upload to synapse
+message("Uploading plots to synpase >>>>>>>>>>")
+system(str_glue("synapse store {clustering_plot_path} --parentId syn52812201"))
+system(str_glue("synapse store {ifc_plot_path} --parentId syn52812201"))
+
+# Remove temp files
+message("Removing temp files >>>>>>>>>>")
+rm_files <- list.files(".", pattern = str_glue("{team}_*"), full.names = TRUE)
+unlink(rm_files)
+# rm_files2 <- list.files("seurat_objects", pattern = str_glue("{team}_*"), full.names = TRUE)
+# unlink(rm_files2)
+gc();gc()
